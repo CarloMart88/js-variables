a68202945f8e235e2e61fdbb4891095d017fce7c
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-01 09:08:35 +0200

    update

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -11,18 +11,18 @@ console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = 'York';
+const citySubstring = city.substring('4 ', '9');
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = 'New Delhi';
+const cityReplaced = city.replace('York', 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = `${city}  ${cityName}`;
+const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
